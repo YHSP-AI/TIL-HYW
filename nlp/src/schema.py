fe7e3af84f4182057c06cfb5b3d159207515184d
Tien-Cheng <tiencheng@protonmail.com>
2024-05-12 21:35:17 +0800

    feat(nlp): set up schema for target output

---
+++ nlp/src/schema.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+class InferenceBackend(str, Enum):
+    HF = "transformers"
+    LCPP = "llamacpp"
+    EXLV2 = "exllamav2"
+
+class TargetFormat(BaseModel):
+    target: str
+    tool: str
+    heading: str = Field(pattern="[0-9][0-9][0-9]")
